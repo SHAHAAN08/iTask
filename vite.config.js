3e95ef35f7b250cdb1b334ba238a766a965d6370
SHAHAAN08 <154593846+SHAHAAN08@users.noreply.github.com>
2025-08-22 11:53:12 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PAth ||  '/iTask'
-});
-
-  
-
+  base: '/', // root deployment on Vercel
+})
